8ec731e1b6d4634fe0762a4f07e3ac9e0cba8177
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-03-08 23:47:47 +0530

    Create Length of C-style string.cpp

---
+++ Length of C-style string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    char str[] = "C++ Programming is awesome";
+
+    // you can also use str.length()
+    cout << "String Length = " << strlen(str);
+
+    return 0;
+}
